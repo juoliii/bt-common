b5dbba3a79ecfb93758a462603db1861dfe8bfc9
juoliii <juoliii@qq.com>
2024-06-24 00:17:18 +0800

    v0.1.1

---
+++ src/main/java/com/bitian/common/spring/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.bitian.common.spring.security;
+
+import com.alibaba.fastjson.JSON;
+import com.bitian.common.dto.ResultJson;
+import com.bitian.common.enums.ResultCode;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        //验证为未登陆状态会进入此方法，认证错误
+        response.setStatus(200);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        PrintWriter printWriter = response.getWriter();
+        String body = JSON.toJSONString(ResultJson.failure(ResultCode.UNAUTHORIZED, authException.getMessage()));
+        printWriter.write(body);
+        printWriter.flush();
+    }
+}
